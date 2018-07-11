9c8a8102d2de1a74608833a03894d240942568bd
alex174 <axchang777@gmail.com>
2018-07-11 11:33:42 -0400

    Create submit.js

---
+++ pages/facebook/client/submit.js
@@ -0,0 +1,21 @@
+Template.fbmakepost.events({
+  "click #submit"(event, instance){
+    var profile = Profiles.findOne({owner:Meteor.userId()});
+    var now = new Date();
+    var text = instance.$("#fb-posttext").val();
+    var postLocation = instance.$("#fb-postloc").val();
+    var postTitle = instance.$("#fb-posttitle").val();
+    var postName = instance.$("#fb-postname").val();
+    var post = {
+      owner:Meteor.userId(),
+      name:profile.name,
+      createdAt: now,
+      text: text,
+      location: postLocation,
+      title: postTitle,
+      filename: postName,
+    };
+    console.dir(post);
+    Posts.insert(post);
+  }
+})
