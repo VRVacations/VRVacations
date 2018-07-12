f4609da18ee505d2a657b9789852f85c70f291a7
zhangk360 <40968774+zhangk360@users.noreply.github.com>
2018-07-12 09:14:35 -0400

    d

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,6 +1,6 @@
 const team =
   [
-    {name:"Alex Chang"    , email: "axchang777@Gmail.com" },
+    {name:"Alex Chang"    , email: "axchang777@gmail.com" },
     {name:"Benson Goldman", email:"bgoldman@stuy.edu"},
     {name:"Elliot Morgenstern", email:"morgensternelliot@gmail.com"},
     {name:"Sean Foody", email:"seanfoody@icloud.com"},
