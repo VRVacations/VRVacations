9f167a9b57692692661eff9fdb26345850906634
zhangk360 <40968774+zhangk360@users.noreply.github.com>
2018-07-12 09:38:28 -0400

    klj

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -4,7 +4,7 @@ const team =
     {name:"Benson Goldman", email:"bgoldman@stuy.edu" , location:"New York"},
     {name:"Elliot Morgenstern", email:"morgensternelliot@gmail.com" , location: "Florida"},
     {name:"Sean Foody", email:"seanfoody@icloud.com" , location: ""},
-    {name:"Kelly Zhang", email:"kelly.zhang721@gmail.com", location: ""},
+    {name:"Kelly Zhang", email:"kelly.zhang721@gmail.com", location: "California"},
     {name:"Fontaine Liu", email:"liufw2@gmail.com", location: ""},
   ];
 
