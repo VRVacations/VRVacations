4a44ee5881812d9d9a94a8a16a5c9c5b06c71ceb
Sean Foody <seanfoody@icloud.com>
2018-07-11 20:40:00 -0400

    changes made

---
+++ pages/facebook/client/fbmyprofile.js
@@ -0,0 +1,13 @@
+Template.fbprofile.helpers({
+  user(){
+    var theProfile = Profiles.findOne({owner:Meteor.userId()});
+    if (!theProfile){
+      var k = Profiles.find().count();
+      Profiles.insert({name:"anonymous"+k,owner:Meteor.userId()});
+    } else {
+    return theProfile
+    }
+   },
+
+
+})
