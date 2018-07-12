a4024b3a67a0065df56dd5d69f39834ce50629ee
Ben Goldman <bgoldman@stuy.edu>
2018-07-12 15:23:05 -0400

    logo, about page

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,11 +1,11 @@
 const team =
   [
-    {name:"Alex Chang"    , email: "axchang777@gmail.com" , location:"Massachusetts" },
+    {name:"Alex Chang"    , email: "axchang777@gmail.com" , location:"Virginia" },
     {name:"Benson Goldman", email:"bgoldman@stuy.edu" , location:"New York"},
     {name:"Elliot Morgenstern", email:"morgensternelliot@gmail.com" , location: "Florida"},
-    {name:"Sean Foody", email:"seanfoody@icloud.com" , location: ""},
+    {name:"Sean Foody", email:"seanfoody@icloud.com" , location: "Massachusetts"},
     {name:"Kelly Zhang", email:"kelly.zhang721@gmail.com", location: "California"},
-    {name:"Fontaine Liu", email:"liufw2@gmail.com", location: ""},
+    {name:"Fontaine Liu", email:"liufw2@gmail.com", location: "Massachusetts"},
   ];
 
 Template.about.helpers(
