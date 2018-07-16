49455a166698c8ce0a1854d6859c5da524420aef
zhangk360 <40968774+zhangk360@users.noreply.github.com>
2018-07-16 10:35:21 -0400

    sub,it

--- pages/facebook/client/submit.js
+++ pages/facebook/client/submit.js
@@ -1,21 +1,37 @@
 Template.submit.events({
   "click #submit"(event, instance){
     var profile = Profiles.findOne({owner:Meteor.userId()});
+    console.log(Profiles.find().count());
     var now = new Date();
     var text = instance.$("#fb-posttext").val();
     var postLocation = instance.$("#fb-postloc").val();
     var postTitle = instance.$("#fb-posttitle").val();
     var postName = instance.$("#fb-postname").val();
-    var post = {
-      owner:Meteor.userId(),
-      name:profile.name,
-      createdAt: now,
-      text: text,
-      location: postLocation,
-      title: postTitle,
-      filename: postName,
-    };
-    console.dir(post);
-    Posts.insert(post);
-  }
+if(postTitle == "") {
+  alert("Please give your post a name. (It makes it easier for you to locate it in the future!)")
+}
+else if (postName=="") {
+  alert("Please provide a link to your VR!")
+}
+else if (postLocation=="") {
+  alert("Please provide a location for your VR!")
+}
+    else{
+
+      var post = {
+        owner:Meteor.userId(),
+        name:profile.name,
+        createdAt: now,
+        text: text,
+        location: postLocation,
+        title: postTitle,
+        filename: postName,
+      };
+      console.dir(post);
+      Posts.insert(post);
+      alert('Your post has been submitted! Go check it out in your feed.')
+
+    }
+    }
+
 })
