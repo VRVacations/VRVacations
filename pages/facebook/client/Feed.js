3bd6d24534019c65f59c682adaf33ad4370649e6
Otherfon <liufw2@gmail.com>
2018-07-16 10:24:33 -0400

    Placed some of the background VR code into Feed.js

--- pages/facebook/client/Feed.js
+++ pages/facebook/client/Feed.js
@@ -5,6 +5,19 @@ Template.fbtimeline.helpers({
     console.dir(myFriendsList);
     return Posts.find({owner:{$in:myFriendsList}},{sort:{createdAt:-1}})
     //return Posts.find();
+    window.addEventListener('load', onVrViewLoad);
+
+    function onVrViewLoad() {
+      // Selector '#vrview' finds element with id 'vrview'.
+      var vrView = new VRView.Player('#vrview', {
+        video: '/url/to/video.mp4',
+        is_stereo: true
+      });
+    }
+    // Selector '#vrview' finds element with id 'vrview'.
+var vrView = new VRView.Player('#vrview', {
+  video: '/url/to/video.mp4',
+  is_stereo: true
+});
   }
 })
-
