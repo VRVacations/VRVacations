d09bc35815bbf6a41eb96d1ed52cc40723805526
Timothy Hickey <tjhickey@brandeis.edu>
2018-07-16 14:46:06 -0400

    fixed our friends

--- pages/facebook/client/friends.js
+++ pages/facebook/client/friends.js
@@ -1,19 +1,34 @@
 
 Template.friends.helpers({
-  users(){return Profiles.find();},
+  users(){
+    console.log('in friends')
+    return Profiles.find();},
+
+
+})
+
+Template.person.helpers({
+
+  isFriend(){
+    console.dir(['in person helper',this])
+    return Friends.findOne(
+      {friend:this.u.owner,
+        owner:Meteor.userId()
+      }
+   )},
+
 })
 
 
-Template.fbperson.helpers({
-  isFriend(){return Friends.findOne({friend:this.u.owner,owner:Meteor.userId()})}
-})
-
-
-Template.fbperson.events({
+Template.person.events({
   "click #js-friend"(event, instance){
+    console.log('in friend')
+    console.dir(this)
+    console.log(JSON.stringify({friend:this.u.owner,owner:Meteor.userId()}))
     Friends.insert({friend:this.u.owner,owner:Meteor.userId()})
   },
   "click #js-defriend"(event,instance){
+    console.log('in defriend')
     var z = Friends.findOne({owner:this.u.owner,friend:Meteor.userId()});
     console.log('removing'); console.dir(z);
     Friends.remove(z._id);
