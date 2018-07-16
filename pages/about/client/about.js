2dfe2c608c28e0852c95705250e955635af5b797
Otherfon <liufw2@gmail.com>
2018-07-16 15:31:46 -0400

    Edited Bios and changed birthplace table header on the friends page

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,10 +1,10 @@
 const team =
   [
     {name:"Alex Chang"    , email: "axchang777@gmail.com" , location:"Virginia", bio: "Alex is the team leader, and is experienced in various languages." },
-    {name:"Benson Goldman", email:"bgoldman@stuy.edu" , location:"New York", bio: "Benson enjoys working with computers in his free time and has experience in HTML.  He also enjoys minecraft and GTA V but dont tell his mother."},
-    {name:"Sean Foody", email:"seanfoody@icloud.com" , location: "Massachusetts", bio: "Sean Foody is an experienced coder, and has experience in strength training."},
+    {name:"Benson Goldman", email:"bgoldman@stuy.edu" , location:"New York", bio: "Benson enjoys working with computers in his free time and has experience in HTML."},
+    {name:"Sean Foody", email:"seanfoody@icloud.com" , location: "Massachusetts", bio: "Sean is an experienced coder, and has experience in strength training."},
     {name:"Kelly Zhang", email:"kelly.zhang721@gmail.com", location: "California", bio: "Kelly has experience working in Unix, Linux, and HTML."},
-    {name:"Fontaine Liu", email:"liufw2@gmail.com", location: "Massachusetts", bio: "Fontaine enjoys coding various projects in his free time."},
+    {name:"Fontaine Liu", email:"liufw2@gmail.com", location: "Massachusetts", bio: "Fontaine has no idea what he's doing, codes through human facilitated machine learning"},
     {name:"Elliot Morgenstern", email:"morgensternelliot@gmail.com" , location: "Florida", bio: "Elliot has experience with graphic design and fencing."}
   ];
 
