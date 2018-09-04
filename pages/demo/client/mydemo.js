2ff73b8f5f076b1898f80e63dec9e18d491506cb
Sean Foody <seanfoody@icloud.com>
2018-09-04 08:14:58 -0400

    Broken

---
+++ pages/demo/client/mydemo.js
@@ -0,0 +1,5 @@
+function onVrViewLoad() {
+  // Selector '#vrview' finds element with id 'vrview'.
+  var vrView = new VRView.Player('#vrview', {
+    image: 'https://i.imgur.com/0cwZyoY.jpg',
+    is_stereo: true
--- pages/virtualreality/client/virtualreality.js
+++
@@ -1,3 +0,0 @@
-Template.virtualreality.helpers({
-  image:function(){return this.filename}
-})
