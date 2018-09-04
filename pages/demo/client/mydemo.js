9ef48c7f760a2894cd250aa9a92c011e26e3fe5e
Sean Foody <seanfoody@icloud.com>
2018-09-04 08:16:05 -0400

    Revert "Broken"
    This reverts commit 2ff73b8f5f076b1898f80e63dec9e18d491506cb.

--- pages/demo/client/mydemo.js
+++
@@ -1,5 +0,0 @@
-function onVrViewLoad() {
-  // Selector '#vrview' finds element with id 'vrview'.
-  var vrView = new VRView.Player('#vrview', {
-    image: 'https://i.imgur.com/0cwZyoY.jpg',
-    is_stereo: true
